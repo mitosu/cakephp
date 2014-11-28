232293ff71b4d0053328aa4141c319bf88faec9f
Miguel Angel Torres <miguel@internetrepublica.com>
2014-11-28 12:40:45 +0100

    Creando el primer controlador y modelo

---
+++ app/Controller/StudentsController.php
@@ -0,0 +1,9 @@
+<?php
+	class StudentsController extends AppController{
+		public $helpers = array('Html', 'Form');
+		public $components = array('Session');
+
+		public function index(){
+
+		}
+	}
