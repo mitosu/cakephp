82d2b7162c8bfb3e220980cccb5f06b953491b5f
Miguel Angel Torres <miguel@internetrepublica.com>
2014-12-16 17:51:38 +0100

    Validación de campos de un formulario y routes.

--- app/Model/Student.php
+++ app/Model/Student.php
@@ -1,4 +1,23 @@
 <?php
+//Tabla en plural y en minúsculas.
+//Clase en singular y en camelcase.
 	class Student extends AppModel{
+		/*Aqui vamos a realizar la validación de los campos de un
+		formulario. Como aquí esta la comunicación con los datos
+		es aquí donde pondremos la logica de validar los datos.*/
 
+		/*Aquí vamos a crear una variable publica llamada validate
+		con las reglas de validación para cada campo. crearemos un
+		array asosiativo con el nombre de input + un array asosiativo
+		el tipo de action en ese caso será 'rule' más el tipo de rule.*/
+
+		/*Se debe poner más de una regla para que aparezca el menssage*/
+		public $validate = array(
+			'name' => array('rule' => 'notEmpty',
+							'rule' => 'isUnique',
+							'message' => 'El nombre de usuario ya existe'),
+			'lastname' => array('rule' => 'notEmpty'),
+			'phone' => array('rule' => 'numeric',
+							'message' => 'Por favor escriba solo números')
+		);
 	}
